a069abfa21bf27770fa26373211429b5b4ff0de6
Khai Duong <duongkhai@gmail.com>
2021-11-06 00:03:14 +0100

    Server and client now have better error handling and send data in streams of packets

--- client.py
+++ client.py
@@ -1,9 +1,14 @@
 import socket
-import sys
+from sys import exit
+import errno
+
+# CONSTANTS
+HEADERSIZE = 10
+
 
 try:
     c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Socket successfuly created")
+    # print("Socket successfuly created")
 except socket.error as err:
     print(f"Socket creation failed with error {err}")
 
@@ -14,6 +19,38 @@ PORT = 8000
 # Connect to server
 try:
     c.connect((HOST, PORT))
-    print("Successfully connected to server.")
+    # print("Successfully connected to server.")
 except ConnectionRefusedError:
     print("[ERRNO 111] Connection refused")
+    exit()
+except OSError as e:
+    if e.errno == errno.ENOTCONN:
+        print(e)
+        exit()
+    else:
+        print("Something went wrong with connecting to host")
+        exit()
+
+
+while True:
+
+    full_msg = ""
+    new_msg = True
+
+    while True:
+        server_msg = c.recv(16)
+
+        if new_msg and server_msg != b'':
+            print(f"new message length: {server_msg[:HEADERSIZE]}")
+            msglen = int(server_msg[:HEADERSIZE])
+            new_msg = False
+
+        full_msg += server_msg.decode("utf-8")
+
+        if len(full_msg) - HEADERSIZE == msglen:
+            print("full msg received")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ""
+
+    print(full_msg)
