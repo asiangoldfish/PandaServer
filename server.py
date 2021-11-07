e774c534e4ea1c12d825ffe53bbfe185acb4bcfa
asiangoldfish <duongkhai@gmail.com>
2021-11-07 15:18:58 +0100

    Database is now its own module of the server. The server is now able to run without the database, but this can be toggled easily with the newly created settings.ini configuration file.

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
+from logging import WARNING
 import socket
 
 from sys import exit
 import errno
 import os
+
+from configparser import ConfigParser
+
+# Enables inputting hidden password
+from getpass import getpass
 
 # Used for handling Python objects in bytes
 import pickle
@@ -10,6 +16,41 @@ from typing import Type
 
 from mysql.connector import connect, Error
 
+# Open config file, for development use
+conf = ConfigParser()
+conf.read("settings.ini")
+# Use the following function to fetch any data from the config file.
+# Ex.: conf_get("default", "host")
+conf_get = lambda x,y: conf.get(x, y)
+
+
+def printc(string: str, code: str,) -> None:
+    """Class for generating strings with colours on terminal. Uses ANSI Escape Codes to generate them.
+    A coloured string must be escaped with RESET, otherwise the chosen colour will persist.
+
+    Args:
+    -----
+        print_string (str): String to print to terminal.
+
+        code (str): Available arguments: OK/Green, WARNING/Yellow, FAIL/Red
+    """
+    OK = "\033[92m"  # Green
+    WARNING = "\033[93m"  # YELLOW
+    FAIL = "\033[91m"  # RED
+    RESET = "\033[0m"  # RESET COLOUR
+
+    testvar = "OK".casefold
+
+    if code.casefold() == "ok" or code.casefold() == "green":
+        colour = OK
+    elif code.casefold() == "warning" or code.casefold() == "yellow":
+        colour = WARNING
+    elif code.casefold() == "fail" or code.casefold() == "red":
+        colour = FAIL
+    else:
+        raise ValueError("Code arg not valid. Check the printc docstring for help.")
+    
+    print("%s%s%s" % (colour, (string), RESET))
 
 class SystemCheck:
     """
@@ -18,59 +59,73 @@ class SystemCheck:
     """
 
     def __init__(self):
-        self.enable_db = False
-
-    def set_db(self, switch: bool):
+        self.run_system_check()
+    
+    def login_db(self) -> None:
         """
-        Choose between enabling or disabling the use of database. When enabled, server admin is prompted
-        with login.
-
-        Args:
-        -----
-            switch (bool): Enable or disable use of database
+        Connect to MYSQL database. 
         """
-        # Raise error if not is not passed by programmer
-        if not isinstance(switch, bool):
-            raise TypeError(
-                "Unable to activate or deactivate database. Value must be bool, not %s." % str(type(switch)))
+        # Section to fetch in the config file
+        section = "Database"
+
+        # Enable auto login for easy testing, debugging or development
+        # WARNING: PLEASE DISABLE AUTO LOGIN in the config file before deployment
+        try:
+            auto_login = conf.getboolean("Database", "auto_login")
+        except ValueError as e:
+            print(f"{e}. Please ensure that the correct value for auto_login is set in the config file.")
+            exit()
+        
+        # Database login credentials
+        # Hard code password for easier time in development. PLEASE DISABLE AUTO LOGIN BEFORE DEPLOYMENT
+        if auto_login:
+            # The value for these can be changed in the config file
+            db_host = conf_get(section, "host")
+            db_user = conf_get(section, "user")
+            db_pass = conf_get(section, "password")
+            db_database = conf_get(section, "database")
+        else:
+            # Prompt server admin for login credentials to database. SAFE WAY TO RUN THE DB SERVER
+            print("Login to MySQL:")
+            db_host = input("Host: ")
+            db_user = input("Username: ")
+            db_pass = getpass("Password: ")
+            db_database = input("Database Name: ")
+        
+
+        # Connect to MySQL database only if login credentials are valid
+        print("\nLogging into MySQL...")
+        try:
+            # Temporary solution
+            mydb = connect(
+                host=db_host,
+                user=db_user,
+                password=db_pass,
+                database=db_database
+            )
+            printc("\nSuccessfully logged into MYSQL!", "ok")
+            # Warn server admin or developer about unsafe login if auto login is enabled.
+            if auto_login:
+                printc("\nDetected unsafe login. Auto login is enabled. Please only use this method for development.\n", "warning")
+
+        # Quit program if wrong login credentials
+        except Error as error:
+            print(error)
+            exit()
+
 
     def run_system_check(self):
-        self. self_enable_db()
-
+        print("Running system check...")
+        if conf.getboolean("Database", "use_db"):
+            self.login_db()
+        printc("System check complete!", "ok")
 
 syschk = SystemCheck()
-syschk.set_db(True)
 
-# Connect to MySQL database only if login credentials are valid
-print("Running system checks...")
-print("Logging into MySQL...")
-try:
-    # Temporary solution
-    mydb = connect(
-        host="localhost",
-        user="test_user",
-        password="test_user123",
-        database="panda",
-    )
-    print("\nSuccessfully logged into MYSQL!")
-    print("Detected unsafe login. Please only use this method for development.")
-
-# Quit program if wrong login credentials
-except Error as error:
-    print(error)
-    exit()
-
-print("\nSystem checks complete!")
-
-# CONSTANTS
-HOST = ""  # Listens to requests coming from the network
-"""try:
-    PORT = int(input("Enter port number: "))
-except KeyboardInterrupt:
-    print("\nTerminating the program.")
-    exit()"""
-PORT = 8080
-HEADERSIZE = 10
+# Host address, port and headersize for messages sent by server
+HOST = conf_get("Default", "host")
+PORT = int(conf_get("Default", "port"))
+HEADERSIZE = int(conf_get("Default", "headersize"))
 
 # Create IPv4 TCP socket if permission is there
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -78,7 +133,7 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Enables reuse of socket
 old_state = server_socket.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
 
-print("\nSocket successfully created")
+printc("Socket successfully created", "ok")
 
 # Enable the SO_REUSE ADDR option
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -93,54 +148,17 @@ except PermissionError as e:
         print("%s most likely due to port number %i" % (e.strerror, PORT))
         exit()
     else:
-        print("Something went wrong when binding socket to port and host")
+        printc("Something went wrong when binding socket to port and host", "fail")
         exit()
 except OSError as e:
-    print("%s. Please change the port." % e)
+    printc("%s. Please change the port." % e, "fail")
     exit()
 
-print("Binding success")
-
-# SQL stuff
-# Fetch datatable
-show_table_query = "DESCRIBE panda_species"
-insert_panda_species = """
-INSERT INTO panda_species (name, species, family, population)
-Values
-    ("Great Panda", "Ailuropoda melanoleuca", "Ursidae", 1527),
-    ("Red Fox", "Ailurus fulgens", "Ailuridae", 312)
-"""
-
-select_panda_species = "SELECT * FROM panda_species"
-
-# Create table if panda_species does not exist
-# Also create rows with some starter data if there are no entries
-
-with mydb.cursor() as cursor:
-    try:
-        cursor.execute(insert_panda_species)
-    except Error:
-        print("Creating table panda_species...", end="")
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS panda_species(
-            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(30) NOT NULL,
-            species VARCHAR(30),
-            family VARCHAR(30),
-            population INT
-        )
-        """)
-        cursor.fetchall()
-        cursor.execute(insert_panda_species)
-        print(" complete!")
-
-    # Print datatable
-    cursor.execute(select_panda_species)
-    cursor.fetchall()
+printc("Binding socket to host and port success!", "ok")
 
 # Become a server socket and listen for clients
 server_socket.listen(5)
-print(f"Socket is listening on port {PORT}...")
+print(f"\nSocket is listening on port {PORT}...\nGo to http://localhost:8080 to open website.")
 
 # Main loop. Keeps the serer running
 while True:
@@ -149,7 +167,7 @@ while True:
         client_socket, address = server_socket.accept()
         #print(f"\nReceived connection from {address}", end="\n\n")
     except KeyboardInterrupt:
-        print("\nSuccessfully terminated the program.")
+        printc("\nSuccessfully terminated the program.", "ok")
         exit()
     except socket.error as msg:
         print("%s" % (msg,))
@@ -207,5 +225,3 @@ while True:
 
     # Close connection
     client_socket.close()
-
-print("Hello")
