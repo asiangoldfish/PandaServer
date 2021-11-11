43bcbe786e6875d40e2364e60c16ddd7aeecc77d
asiangoldfish <duongkhai@gmail.com>
2021-11-11 18:22:36 +0100

    Restructure HttpServer init method and make conf.get methods more readable.

--- pandahttp/httpserver.py
+++ pandahttp/httpserver.py
@@ -16,6 +16,18 @@ from .terminal import printc
 class HttpServer:
 
     def __init__(self, host="localhost", port=8080, infinite_connection=True, concurrent_clients=5) -> None:
+        # Default server settings
+        self.host = host
+        self.port = port
+        self.infinite_connection = infinite_connection
+
+        # Amount of clients to listen for before being inavailable
+        self.concurrent_clients = concurrent_clients
+
+        # Start server and create socket
+        self.server_socket = self.start_server()
+
+    def start_server(self):
         """Asign required host settings to start the Http server
 
         On initializing an instance, the class will initialize the server by creating a socket, binding it to
@@ -27,16 +39,10 @@ class HttpServer:
             infinite_connection (bool, optional): If false, the server will terminate after responding once to one client. Defaults to True.
             concurrent_clients (int, optional): Amount of clients that can be connected to server concurrently. Defaults to 5.
         """
-        # Default server settings
-        self.host = host
-        self.port = port
-        self.infinite_connection = infinite_connection
-        # Amount of clients to listen for before being inavailable
-        self.concurrent_clients = concurrent_clients
 
         # Create socket
         try:
-            self.server_socket = socket.socket(
+            server_socket = socket.socket(
                 socket.AF_INET, socket.SOCK_STREAM)
         except Error as e:
             print(e)
@@ -45,13 +51,13 @@ class HttpServer:
         # Enable reusing socket after terminating the program.
         # If this is not included, there's a chance that an error will be raised
         # after running the program again on the same socket.
-        self.server_socket.setsockopt(
+        server_socket.setsockopt(
             socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         # Bind socket to host (domain/ip) and port
         # Raise error if unsuccessful and terminate the program
         try:
-            self.server_socket.bind((self.host, self.port))
+            server_socket.bind((self.host, self.port))
             printc("Binding socket to host and port success!", "ok")
         except PermissionError as e:
             # Permission denied
@@ -64,7 +70,7 @@ class HttpServer:
             exit()
 
         # Listen to clients
-        self.server_socket.listen(self.concurrent_clients)
+        server_socket.listen(self.concurrent_clients)
 
         # Print message on where the server is listening on
         if self.host == "":
@@ -75,25 +81,25 @@ class HttpServer:
             print(
                 f"\nSocket is listening on port {self.port}...\nGo to http://localhost:8080 to open website.")
 
+        return server_socket
 
-def request_handle(request: str) -> str:
-    """Processes requests and handle them.
+    def request_handle(request: str) -> str:
+        """Processes requests and handle them.
 
-    Process requests and return a HTTP response code and header.
+        Process requests and return a HTTP response code and header.
 
-    Args:
-        request (str): request string to process
+        Args:
+            request (str): request string to process
 
-    Returns:
-        str: Header returned and to be returned to client.
-    """
-    pass
+        Returns:
+            str: Header returned and to be returned to client.
+        """
+        pass
 
+    def request_ok() -> str:
+        """Received request from client is ok.
 
-def request_ok() -> str:
-    """Received request from client is ok.
-
-    Returns:
-        str: Header to be sent back to client.
-    """
-    pass
+        Returns:
+            str: Header to be sent back to client.
+        """
+        pass
